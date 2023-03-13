73b91b06c14176d27c16d1016e1757730349cb7e
dittygoops <aditya.gup16@gmail.com>
2023-03-13 10:03:21 -0700

    Add blueH3

--- using_js_lesson/script.js
+++ using_js_lesson/script.js
@@ -4,3 +4,8 @@ const redP = document.createElement('p');
 redP.textContent = "Hey I'm red!";
 redP.style.color = 'red';
 body.appendChild(redP);
+
+const blueH3 = document.createElement('h3');
+blueH3.textContent = "I'm a blue h3!";
+blueH3.style.color = 'blue';
+body.append(blueH3);
