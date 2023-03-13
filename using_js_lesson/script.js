17ceeaa30693903986f342c992454ebb5d2176c9
dittygoops <aditya.gup16@gmail.com>
2023-03-13 10:07:26 -0700

    Add div

--- using_js_lesson/script.js
+++ using_js_lesson/script.js
@@ -9,3 +9,13 @@ const blueH3 = document.createElement('h3');
 blueH3.textContent = "I'm a blue h3!";
 blueH3.style.color = 'blue';
 body.append(blueH3);
+
+const div = document.createElement('div');
+const h1 = document.createElement('h1');
+const p = document.createElement('p');
+h1.textContent = "I'm in a div";
+p.textContent = "ME TOO!";
+div.append(h1, p);
+div.style.border = '1px solid black';
+div.style.backgroundColor = 'pink';
+body.append(div);
